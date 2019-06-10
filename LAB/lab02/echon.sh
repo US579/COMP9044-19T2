4a7277bb6a8dd1950e764fb76ac51a1c573e0896
z5137189 <z5137189@cse.unsw.edu.au>
2019-06-10 16:11:28 +1000

    change format

--- LAB/lab02/echon.sh
+++ LAB/lab02/echon.sh
@@ -4,13 +4,13 @@ str=$2
 
 if ( test "$#" != 2 );
 then 
-   echo "Usage: ./"$0" <number of lines> <string>"
+   echo "Usage: "$0" <number of lines> <string>"
    exit 1
 fi 
 
 if !( echo "$1" | egrep ^[[:digit:]]+) 1>/dev/null;
 then
-   echo ./"$0": argument 1 must be a non-negative integer
+   echo "$0": argument 1 must be a non-negative integer
    exit 1
 fi
 
