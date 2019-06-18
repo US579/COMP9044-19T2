a2f5ad57a42aabf1881723444d2bc0f75fa5fa27
z5137189 <z5137189@cse.unsw.edu.au>
2019-06-18 14:41:51 +1000

    commit

--- LAB/lab03/jpg2png.sh
+++ LAB/lab03/jpg2png.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+suffix=".png"
 for file in *.jpg
+do
+   name=`echo $file | cut -d"." -f1`
+   png=$name$suffix
+   if test -e "$png"
+   then 
+     echo "$png already exists"
+     exit 1
+   else
+     convert "$file" "$png" && rm "$file"
+   fi
+done
