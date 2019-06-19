35091f4e58ce62560673189e4d1adb59c5820f58
us579 <wzus579@gmail.com>
2019-06-19 14:50:50 +1000

    add_challenge

--- LAB/lab03/create_music.sh
+++ LAB/lab03/create_music.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+sample="$1"
+dir="$2"
+
+wget -q -O- 'https://en.wikipedia.org/wiki/Triple_J_Hottest_100?action=raw'|
+while read line
+do
+    if [[ "$line" =~ (Triple J Hottest 100, )+[0-9]{4} ]]
+    then
+        temp=`echo "$line" | egrep -o "(Triple J Hottest 100, )+[0-9]{4}"`
+        album=`echo "$temp" | sed "s/.*\[\[//;s/\|.*//"`
+        echo "$album"
+        year=`echo $album | sed 's/.*, //'`
+        echo $year
+        mkdir -p -m 755 "$dir/Triple J Hottest 100, $year"
+    fi
+done
+
