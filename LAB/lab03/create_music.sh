a03f6b44b976b6bca53f6345ebac9d28188e5609
us579 <wzus579@gmail.com>
2019-06-19 20:32:04 +1000

    add_challenge

--- LAB/lab03/create_music.sh
+++ LAB/lab03/create_music.sh
@@ -5,25 +5,32 @@ dir="$2"
 
 wget -q -O- 'https://en.wikipedia.org/wiki/Triple_J_Hottest_100?action=raw'|
 while read line
-do
+do   
     if [[ "$line" =~ (Triple J Hottest 100, )+[0-9]{4} ]]
     then
+        echo "$line"
         temp=`echo "$line" | egrep -o "(Triple J Hottest 100, )+[0-9]{4}"`
         album=`echo "$temp" | sed "s/.*\[\[//;s/\|.*//"`
         year=`echo $album | sed 's/.*, //'`
-        #mkdir -p -m 755 "$dir/Triple J Hottest 100, $year"
+        echo "$year"
+        mkdir -p -m 755 "$inter"
     fi
     time=1
-    while read line && test $time -lt 10
+    while read line && test $time -le 10
     do
         if !([[ "$line" =~ ^# ]])
         then
             continue
-        else
-            temp=`echo "$line" |sed "s/[^[]*//" | tr -d '[]"#'`
-
         fi
-
-done
+            tem=`echo "$line" |sed "s/[^[]*//" | tr -d '[]"#'`
+  #          echo "$tem"
+            artist=`echo "$tem" | sed 's/–.*//' | sed "s/^[ ]*//;s/[ ]*$//"`
+            title=`echo "$tem" | sed 's/.*–//'| sed "s/^[ ]*//;s/[ ]*$//"`
+#            echo "$artist"
+ #           echo "$title"
+            file="$inter/$time - $title - $artist.mp3"
+#            cp -p "$sample" "$file"
+            time=$(( time + 1 ))    
+    done
 done
 
