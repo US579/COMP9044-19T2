4f1afc9c26f9b92898e0aa13ee57ff653bbe3363
us579 <wzus579@gmail.com>
2019-06-25 06:51:33 +1000

    add_lab04

--- LAB/lab04/Exercise3/tail.pl
+++ LAB/lab04/Exercise3/tail.pl
@@ -8,36 +8,34 @@ if (@ARGV>0 && $ARGV[0] =~ /-\d+/){
 }
 
 
-shift @ARGV;
-if (@ARGV==0){
-   @line=<>;
-   $count=@line-$num;
-   print "$count\n";
-   while(<>){
-   if ($count<$num){
-     $count++;
-     next;
-    }else
-    {
-      print;
+
+if (@ARGV==1){
+  open my $f, '<', $ARGV[0] or die "$0: can't open $ARGV[0]\n";
+  @line=<$f>;
+  shift @ARGV;
+  while(@line>$num){
+    shift @line;
+  }
+  foreach $l (@line){
+    print $l;
+  }
+}
+
+else{
+  foreach $file (@ARGV){
+    open my $fi, '<', $file or die "$0: can't open $file\n";
+    if (@ARGV > 1){
+        print "==> $file <==\n";
+      }
+      @line=<$fi>;
+      while (@line>$num){
+        shift @line;
+      }
+      foreach $l (@line){
+        print $l;
+      }
+      close $fi;
     }
-
-}
-}else{
-   foreach $file (@ARGV){
-
 }
 
 
-}
-
-
-
-#foreach $file (@ARGV){
-#   if ( ! -e $file ){
-#      print "$0: can't open FileName"
-#}else{
-#   
-#   print "$file\n";
-#}
-#}
