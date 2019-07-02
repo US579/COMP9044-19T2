347e7425f16597df3a63dbd56143efd8cd0dc5de
us579 <wzus579@gmail.com>
2019-07-02 17:07:01 +1000

    add week05 lab exercise1

--- LAB/lab05/backup.sh
+++ LAB/lab05/backup.sh
@@ -6,19 +6,21 @@ if !( test -e ".$name.0")
 then 
     cat $name > ".$name.0"
     exit 0
+else
+	for i in `ls -a`
+	do  
+	    if ( echo $i | egrep ".$name.[0-9]*" ) > /dev/null
+	    then
+	        seq=`echo $i | cut -d. -f4`
+	        num="$num $seq"
+	    fi
+	done
 fi
 
-for i in `ls -a`
-do
-    echo "$i"
-    if ( echo $i | egrep ".$name.[0-9]*" )
-    then
-        seq=`echo $i | cut -d. -f4`
-        num="$num $seq"
-    fi
-done
 
-new=`echo $num | tr " "  "\n" | sort -n | tail -1`
+new=`echo $num | tr " "  "\n" | sort -n | tail -1` > /dev/null
+
+new=$((new +1 ))
 
 cat $name > ".$name.$new"
 
