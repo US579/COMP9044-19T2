2d2d025e764fe39e33690ba14050818c4a267118
us579 <wzus579@gmail.com>
2019-07-02 20:24:43 +1000

    add week05 lab exercise2

--- LAB/lab05/Exercise02/backup.pl
+++ LAB/lab05/Exercise02/backup.pl
@@ -1,1 +1,47 @@
 #!/usr/bin/perl -w
+
+
+if (@ARGV==0){
+	print "backup.pl error: backup.pl <file>";
+}
+
+
+$name=$ARGV[0];
+@list=();
+if ( ! -e ".$name.0"){
+	open F ,"<", "$name" or die "cannot open '$name'";
+	foreach $line (<F>){
+		push @list , $line;
+	}
+	close F;
+	open A,">" , ".$name.0" or die "cannot open '$name'";
+	foreach $i (@list){
+		print A "$i";
+	}
+	close A;
+	print "Backup of '$name' saved as '.$name.0'\n";
+}
+else
+{	
+	@arry=();
+	foreach $file (glob ".*"){
+		if ( $file =~ /\.$name\.[0-9]+/ ){
+			$file =~ s/.*\.//g;
+			push @arry ,$file;
+
+		}
+	}
+	$seq = (reverse sort @arry)[0];
+	$seq += 1;
+	open F ,"<", "$name" or die "cannot open '$name'";
+	foreach $line (<F>){
+		push @list , $line;
+	}
+	close F;
+	open A,">" , ".$name.$seq" or die "cannot open '$name'";
+	foreach $i (@list){
+		print A "$i";
+	}
+	close A;
+	print "Backup of '$name' saved as '.$name.$seq'\n";
+}
