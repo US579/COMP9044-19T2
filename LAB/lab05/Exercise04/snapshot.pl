145e784b0659187677379b8c279b7411a8ba9954
us579 <wzus579@gmail.com>
2019-07-03 00:54:47 +1000

    add week05 lab exercise4

---
+++ LAB/lab05/Exercise04/snapshot.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl -w
+
+
+use File::Copy;
+
+
+sub lastseq{
+	@arry=();
+	foreach $i (glob ".*"){
+		$i =~ s/.*\.//;
+		push @arry, $i;
+	}
+	$seq=(reverse sort @arry)[0];
+	$seq+=1;
+	return $seq;
+}
+
+
+if (@ARGV==1 && $ARGV[0] eq 'save'){
+	if ( ! -d ".snapshot.0" ){
+		mkdir( ".snapshot.0"  ) or die "cannot create '.snapshot.0' !";
+		foreach $f1 (glob "*"){
+			if ($f1 !~ ".*\.pl"){
+		copy("$f1",".snapshot.0");
+		}
+		}	
+		print "Creating snapshot 0\n";
+	}else
+	{
+		$num=&lastseq();
+		mkdir( ".snapshot.$num"  ) or die "cannot create '.snapshot.$num' !";
+		foreach $f2 (glob "*"){
+			if ($f2 !~ ".*\.pl"){
+		copy("$f2",".snapshot.$num");
+			}
+		}	
+		print "Creating snapshot $num\n";
+	}
+}
+
+
+if (@ARGV==2 && $ARGV[0] eq "load"){
+	$num=&lastseq();
+	mkdir( ".snapshot.$num"  ) or die "cannot create '.snapshot.$num' !";
+	foreach $f3 (glob "*"){
+		if ($f3 !~ ".*\.pl"){
+	copy("$f2",".snapshot.$num");
+		}
+	}	
+	print "Creating snapshot $num\n";
+
+	foreach $f4 (glob " ./.snapshot.$ARGV[1]/*"){
+	copy("$f4","./");
+	}	
+	print "Creating snapshot $ARGV[1]\n"
+}
+
+
+
+
+
+
+
+
+
