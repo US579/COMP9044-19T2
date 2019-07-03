5327ed782b2fc7fd91044f5dcbd2b956fdf4f7da
us579 <wzus579@gmail.com>
2019-07-03 17:29:53 +1000

    add week05 lab challenge

--- LAB/lab05/challenge/perl_print.pl
+++ LAB/lab05/challenge/perl_print.pl
@@ -1,13 +1,11 @@
 #!/usr/bin/perl -w
 
 $str=$ARGV[0];
-# print $str;
 
 print "#!/usr/bin/perl -w\n\n";
-#print F '#!/usr/bin/perl -w\n";
 print "print \"";
 foreach my $x (split (//,$str)) {
-	printf "\\x%x", ord($x);
+	printf "\\x%02x", ord($x);
 }
 print "\\n\";\n"
 
