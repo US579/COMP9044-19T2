d34d7ead9daa640d7120069150f1e7c24652fa68
us579 <wzus579@gmail.com>
2019-07-05 23:48:32 +1000

    add weekly test

--- weekly_test/week05/replace_digits.pl
+++ weekly_test/week05/replace_digits.pl
@@ -1,1 +1,14 @@
 #!/usr/bin/perl -w
+
+$filename=$ARGV[0];
+
+open F, "<", "$filename" or die "can not open!";
+# foreach $line ( F ){
+# 	print "$line\n";
+# }
+
+while (<F>){
+    $_ =~ tr /[0-9]*/#/;
+	print $_;
+}
+close F;
