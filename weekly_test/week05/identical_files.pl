245cdc1c8353fed3c68a2a5925cc2e7ec92ba047
us579 <wzus579@gmail.com>
2019-07-06 00:38:32 +1000

    add weekly test

---
+++ weekly_test/week05/identical_files.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+use File::Compare;
+
+if (@ARGV == 0){
+    print "Usage: ./identical_files.pl <files>\n";
+    exit 1;
+}
+
+
+@arg=();
+foreach (@ARGV){
+    push @arg ,$_;
+}
+
+foreach  $i (0..$#arg-1){
+
+
+$issame=1;
+foreach $f1 (@arg){
+    foreach $f2 (@arg){
+        if ( compare($f1,$f2) != 0){
+            $issame = 0;
+        }
+    }
+}
+if ($issame == 1){
+    print "All files are identical\n";
+    exit 0;
+}
+
+
+if (compare($arg[$i],$arg[$i+1])!=0){
+        print "$arg[$i+1] is not identical\n";
+        exit 1;
+    }
+}
