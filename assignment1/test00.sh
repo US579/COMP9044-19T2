79bd30bd85c8e43f74d806249dc97bb011707ab4
z5137189 <z5137189@cse.unsw.edu.au>
2019-07-10 08:28:30 +1000

    commit

--- assignment1/test00.sh
+++ assignment1/test00.sh
@@ -1,48 +1,38 @@
 #!/bin/sh
 
-if ([ -d ".legit" ]);
-then
-    rm -rf ".legit/"
-fi
+
+rm -rf ".legit/"
 
 # your output
-echo "test1 (add files (and status))"
-sh legit-init >>out1.txt
-touch a b c 
+echo -e "\e[36mTEST1 init add status \n\e[0m"
+
+touch a b c d e f
 touch out1.txt
 touch out2.txt
-sh legit-add a b c
+sh legit-init >> out1.txt
+sh legit-add a b c d e f
 sh legit-commit -m "hello" >>out1.txt
-sh legit-status >>out1.txt
-rm -rf ".legit" &>/dev/null
-sh legit-init >>out1.txt
-sh legit-add a b d e
-sh legit-commit -m "123" >>out1.txt
 sh legit-status >>out1.txt
 rm -rf ".legit" &>/dev/null
 
 # correct output
 2041 legit-init >>out2.txt
-2041 legit-add a b c d e f
-2041 legit-commit -m "123" >>out2.txt
-2041 legit-status >>out2.txt
-rm -rf ".legit" &>/dev/null
-2041 legit-init >>out2.txt
-2041 legit-add a b d e
-2041 legit-commit -m "123" >>out2.txt
+2041 legit-add a b c d e f 
+2041 legit-commit -m "hello" >>out2.txt
 2041 legit-status >>out2.txt
 rm -rf ".legit" &>/dev/null
 
 if cmp -s "out1.txt" "out2.txt"
 then
-	echo -e "\e[32mALL TEST PASSED\e[0m"
+	echo -en "\e[32mALL TESTS PASSED\n\e[0m"
 else
-	echo "\e[31mautotest failed, different output than expected see below\e[0m"
-	echo "=================> your output <============================"
+	echo -e "\e[31mautotest failed\e[0m"
+	echo "\e[31m===================> your output <============================\n\e[0m"
 	cat out1.txt
-	echo "=================> expected output <========================"
+	echo "\e[32m===================> expected output <========================\n\e[0m"
 	cat out2.txt
 fi
-rm a b c d e f
+
+rm a b c d e f 
 rm out1.txt
 rm out2.txt
