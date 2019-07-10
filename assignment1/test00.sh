d6c44acac66bccbadb4989fd03bb34e7aa7866bc
us579 <wzus579@gmail.com>
2019-07-10 20:35:40 +1000

    modify legit-add

--- assignment1/test00.sh
+++ assignment1/test00.sh
@@ -1,47 +1,85 @@
 #!/bin/sh
 
+legitdir='.legit'
+sub_legitdir='.git'
+fullpath="$legitdir/$sub_legitdir"
+indexdir="objects"
 
-std='Initialized empty legit repository in .legit';
-std1='';
+
+echo -en "\e[37mALL TESTS BEGIN\e[0m"
+
 #test legit-add
 
-
-echo -en "\e[37mALL TESTS BEGIN\n\e[0m"
-
 ## 1 init repo
-out1=$( sh legit-init )
 
+std1='Initialized empty legit repository in .legit';
 
+out1=$(sh legit-init )
 
 ## 1 init 
 
-if [ "$std" = "$out1" && test -d ".legit" ]; then
-  echo -en "\e[32minit PASSED\n\e[0m"
+if [ "$std1" = "$out1" ] && [ -d "$legitdir" ]
+then
+  echo -en "\e[32minit PASSED\e[0m"
 else
-  echo -en "\e[31minit failed\n\e[0m"
+  echo -en "\e[31minit failed\e[0m"
 fi
 
 
-# 2 add a adding it to index and staged
+# 2 add file a
+
 echo "hello" > a
 sh legit-add a
-if [ test -e "$" ]; then
-  echo -en "\e[32minit PASSED\n\e[0m"
+
+if [ -e "$fullpath/$indexdir/a" ]
+then
+  echo -en "\e[32minit PASSED\e[0m"
 else
-  echo -en "\e[31minit failed\n\e[0m"
+  echo -en "\e[31minit failed\e[0m"
+fi
+
+# 3 add file that not exists
+
+std2="legit-add: error: can not open 'b'"
+out2=$( sh legit-add b )
+if [ "$std2" = "$out2" ]
+then
+  echo -en "\e[32madd non-exist PASSED\e[0m"
+else
+  echo -en "\e[31madd non-exist failed\e[0m"
+fi
+
+# add same file
+out2=$( sh legit-add a )
+if (diff "a" "$fullpath/$indexdir/a" > /dev/null )
+then
+  echo -en "\e[32madd same file PASSED\e[0m"
+else
+  echo -en "\e[31madd same file failed\e[0m"
 fi
 
 
-# 2 add a   adding it to index and staged
+# add same file but changing after been added
+out2=$( sh legit-add a )
+echo asd >> a 
+if ! (diff "a" "$fullpath/$indexdir/a" > /dev/null )
+then
+  echo -en "\e[32madd same file PASSED(changed)\e[0m"
+else
+  echo -en "\e[31madd same file failed(changed)\e[0m"
+fi
+
+
+
+
+
 echo ""
 rm -rf .legit
 
 
-
-
-if [ $? == 0 ]
-then
-	echo -en "\e[32mALL TESTS PASSED\n\e[0m"
-else
-	echo -e "\e[31mautotest failed\e[0m"
-fi
+# if [ $? == 0 ]
+# then
+# 	echo -en "\e[32mALL TESTS PASSED\n\e[0m"
+# else
+# 	echo -e "\e[31mautotest failed\e[0m"
+# fi
