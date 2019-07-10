c89dd91ee8ecf452ca349f334626e78aeec54d0e
us579 <wzus579@gmail.com>
2019-07-10 23:41:20 +1000

    add test02

--- assignment1/test02.sh
+++ assignment1/test02.sh
@@ -42,7 +42,6 @@ fi
 sh legit-init > /dev/null
 std1="legit-log: error: your repository does not have any commits yet"
 out1=$(sh legit-log )
-echo $out1
 if [ "$std1" = "$out1" ]
 then
   echo -en "\e[32mrepo dir exists , but no commit PASSED\n\e[0m"
@@ -50,5 +49,37 @@ else
   echo -en "\e[31mrepo dir exists , but no commit failed\n\e[0m"
 fi
 
+# 3. has one commit 
+echo "hello" > a
+sh legit-add a
+sh legit-commit -m "commit" > /dev/null
+std1="0 commit"
+out1=$(sh legit-log )
+if [ "$std1" = "$out1" ]
+then
+  echo -en "\e[32m has one commit PASSED\n\e[0m"
+else
+  echo -en "\e[31mr has one commit failed\n\e[0m"
+fi
+rm -rf .legit
+
+# 3. has two commit , must be descending order
+sh legit-init > /dev/null
+echo "hello" > a
+sh legit-add a
+sh legit-commit -m "commit" > /dev/null
+echo "hello" > b
+sh legit-add b
+sh legit-commit -m "commit" > /dev/null
+std1="1 commit
+0 commit"
+out1=$(sh legit-log )
+echo $out1
+if [ "$std1" = "$out1" ]
+then
+  echo -en "\e[32m has two commit PASSED\n\e[0m"
+else
+  echo -en "\e[31mr has two commit failed\n\e[0m"
+fi
 echo ""
 rm -rf .legit
