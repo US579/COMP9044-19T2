745bf99c88c3fd2c57150f8b6a47bf1ad6951261
us579 <wzus579@gmail.com>
2019-07-10 21:41:55 +1000

    add test00

--- assignment1/test00.sh
+++ assignment1/test00.sh
@@ -11,20 +11,16 @@ echo -en "\e[37mALL TESTS BEGIN\n\e[0m"
 #test legit-add
 
 ## 1 init repo
-
 std1='Initialized empty legit repository in .legit';
-
 out1=$(sh legit-init )
 
 ## 1 init 
-
 if [ "$std1" = "$out1" ] && [ -d "$legitdir" ]
 then
   echo -en "\e[32minit PASSED\n\e[0m"
 else
   echo -en "\e[31minit failed\n\e[0m"
 fi
-
 
 # 2 add file a
 
@@ -49,7 +45,7 @@ else
   echo -en "\e[31madd non-exist failed\n\e[0m"
 fi
 
-# add same file
+# 4 add same file
 out2=$( sh legit-add a )
 if (diff "a" "$fullpath/$indexdir/a" > /dev/null )
 then
@@ -59,7 +55,7 @@ else
 fi
 
 
-# add same file but changing after been added
+# 5 add same file but changing after been added
 out2=$( sh legit-add a )
 echo asd >> a 
 if ! (diff "a" "$fullpath/$indexdir/a" > /dev/null )
@@ -69,7 +65,7 @@ else
   echo -en "\e[31madd same file failed(changed)\n\e[0m"
 fi
 
-# add file with invalid file name
+# 6 add file with invalid file name
 
 echo "hello" > _a
 out3=$( sh legit-add _a )
@@ -81,7 +77,7 @@ else
   echo -en "\e[31madd invalid name file failed\n\e[0m"
 fi
 
-#  .legit dir is not exit when adding file
+# 7 .legit dir is not exit when adding file
 rm -rf "$legitdir"
 out4=$(sh legit-add a )
 std4="legit-add: error: no $legitdir directory containing legit repository exists"
@@ -93,8 +89,18 @@ else
 fi
 sh legit-init >/dev/null
 
-# 
 
+# 8 add file not exist in current dir but exist in index dir
+echo "hello" > b
+sh legit-add b >/dev/null
+rm b
+sh legit-add b >/dev/null
+if [ -e "$fullpath/$indexdir/b" ]
+then
+  echo -en "\e[31madd file not exist in current dir but exist in index dir failed\n\e[0m"
+else
+  echo -en "\e[32madd file not exist in current dir but exist in index dir PASSED\n\e[0m"
+fi
 
 
 
