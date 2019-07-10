f8285f79600121134d9259104e85560c7538b227
us579 <wzus579@gmail.com>
2019-07-11 09:09:07 +1000

    add lab exercise5

--- LAB/lab06/Exercise05/identify_artist.pl
+++ LAB/lab06/Exercise05/identify_artist.pl
@@ -12,6 +12,25 @@ else{
 $dir = shift @ARGV;
 $dir =~ s/0/\*/g;
 
+
+foreach $file (glob "lyrics/*.txt") {
+    open F1 , "<" ,"$file" or die "file can not open!\n";
+    my @content = <F1>;
+    close F1;
+    $file =~ s/\.txt//;
+    $file =~ s/.*\///;
+    $file =~ s/_/ /g;
+    foreach $line (@content){
+        @words = split /[^a-z]/i,$line;
+        foreach $word (@words){
+            $word_times{$file}{$word}++;
+            $word_total{$file}++;
+        }
+        }
+    }   
+
+
+@name = keys %word_total;
 foreach $f (glob "$dir"){
 	open F , "<","$f" or die "can not open";
     @L=<F>;
@@ -20,47 +39,23 @@ foreach $f (glob "$dir"){
     foreach $line (@L){
 		@arr1 = split /\W+/i , $line;
 		foreach $ww ( @arr1 ){
-			%has=&log_prob($ww);
-			foreach $k (keys %has){
-    			$buff{$k} += $has{$k};
-				}
+            foreach $n (@name){
+                $buff{$n}+=log((($word_times{$n}{$ww}||0)+1)/$word_total{$n});
+                }
 			}
 		}
         if ($flag == 1){
-            foreach my $x (reverse sort{$buff{$a} <=> $buff{$b}} keys %buff) {
+            foreach  $x (reverse sort{$buff{$a} <=> $buff{$b}} keys %buff) {
                 printf "$f: log_probability of %.1f for $x\n",$buff{$x};
             }
         }
-        foreach my $x (reverse sort{$buff{$a} <=> $buff{$b}} keys %buff) {
+        foreach  $x (reverse sort{$buff{$a} <=> $buff{$b}} keys %buff) {
                 printf "$f most resembles the work of $x (log-probability=%.1f)\n",$buff{$x};
                 last;
             }
     }
 
 
-sub log_prob{
-	my ($word) = @_;
-	foreach $file (glob "lyrics/*.txt") {
-	    open F1 , "<" ,"$file" or die "file can not open!\n";
-        my @content = <F1>;
-        close F1;
-        $word_times = 0;
-        $all_len = 0;
-		foreach $line (@content){
-            $linecp = $line;
-            $wc = ($line =~ s/\b$word\b//ig );
-            $tc = ($linecp =~ s/\W?[a-z]+\W?//ig);
-            $word_times += $wc;
-            $all_len += $tc;
-        }
-		$var=log(($word_times+1)/$all_len); 
-		$file =~ s/\.txt//;
-		$file =~ s/.*\///;
-	    $file =~ s/_/ /g;
-	    $dic{$file}=$var;
-	}
-	return %dic;
-}
 
 
 
