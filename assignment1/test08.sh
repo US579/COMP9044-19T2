ef04956cb899cd14e22e50809bca89dee5bfed6e
us579 <wzus579@gmail.com>
2019-07-11 21:27:19 +1000

    add test08

--- assignment1/test08.sh
+++ assignment1/test08.sh
@@ -14,7 +14,7 @@ fi
 
 echo -en "\e[35mALL TESTS BEGIN\n\e[0m"
 
-# Test for test for legit-branch 
+# Test for test for legit-checkout
 
 ## 1 init repo
 std1='Initialized empty legit repository in .legit';
@@ -28,79 +28,115 @@ else
   echo -en "\e[31minit failed\n\e[0m"
 fi
 
-
-# 2. error check
-std1='legit-branch: error: your repository does not have any commits yet';
-out1=$( perl legit-branch new )
-
-if [ "$std1" = "$out1" ] 
-then
-  echo -en "\e[32merror check PASSED\n\e[0m"
-else
-  echo -en "\e[31merror check failed\n\e[0m"
-fi
-
-# 2. branch name check 
-std1="legit-branch: error: invalid branch name '.new'";
-touch a
-sh legit-add a 
-sh legit-commit -m 'commit' > /dev/null
-out1=$( perl legit-branch .new )
-if [ "$std1" = "$out1" ]
-then
-  echo -en "\e[32mbranch name check PASSED\n\e[0m"
-else
-  echo -en "\e[31mbranch name check commitfailed\n\e[0m"
-fi
-rm -rf $legitdir 
-
-# 3. create a new branch
+# 2. the branch switch to is not exist
+std1="legit-checkout: error: unknown branch 'new'"
 sh legit-init > /dev/null
 touch a 
 sh legit-add a
-sh legit-commit -m "commit"
-perl legit-branch new 
-if [ -e "$fullpath/Branch.new" ]
-then
-  echo -en "\e[32mcreate a new branch PASSED\n\e[0m"
-else
-  echo -en "\e[31mcreate a new branch commitfailed\n\e[0m"
-fi
-
-# 4. delete a master
-std1="legit-branch: error: can not delete branch 'master'";
-perl legit-branch -d master > .a 
+sh legit-commit -m "commit" > /dev/null
+perl legit-checkout new > .a
 out1=`cat .a`
-rm .a
-if [ "$std1" = "$out1" ] && [ -e "$fullpath/Branch.master" ]
-then
-  echo -en "\e[32mdelete a master PASSED\n\e[0m"
-else
-  echo -en "\e[31mdelete a master failed\n\e[0m"
-fi
-
-# 4. list all the branch 
-rm -rf .legit
-sh legit-init > /dev/null
-touch a 
-sh legit-add a 
-sh legit-commit -m "commit"
-perl legit-branch new
-perl legit-branch new1
-perl legit-branch new2
-perl legit-branch > .a
-out1=`cat .a`
-std1="master
-new
-new1
-new2"
 if [ "$std1" = "$out1" ]
 then
-  echo -en "\e[32mlist all the branch PASSED\n\e[0m"
+  echo -en "\e[32mthe branch switch to is not exist PASSED\n\e[0m"
 else
-  echo -en "\e[31mlist all the branch failed\n\e[0m"
+  echo -en "\e[31mthe branch switch to is not exist failed\n\e[0m"
 fi
 rm .a
+rm -rf .legitdir
+
+
+# 2. switch to  new branch 
+std1="Switched to branch 'new'"
+sh legit-init > /dev/null
+touch a 
+sh legit-add a
+sh legit-commit -m "commit" > /dev/null
+perl legit-branch new 
+perl legit-checkout new > .a
+out1=`cat .a` 
+if [ "$std1" = "$out1" ]
+then
+  echo -en "\e[32mswitch to  new branch PASSED\n\e[0m"
+else
+  echo -en "\e[31mswitch to  new branch failed\n\e[0m"
+fi
+rm .a
+rm -rf $legitdir
+
+# 3. check wether the branch is already in that branch
+std1="Already on 'new'"
+sh legit-init > /dev/null
+touch a 
+sh legit-add a
+sh legit-commit -m "commit" > /dev/null
+perl legit-branch new > /dev/null
+perl legit-checkout new  > /dev/null
+perl legit-checkout new > .a 
+out1=`cat .a`
+if [ "$std1" = "$out1" ]
+then
+  echo -en "\e[32mscheck wether the branch is already in that branch PASSED\n\e[0m"
+else
+  echo -en "\e[31mcheck wether the branch is already in that branch failed\n\e[0m"
+fi
+rm .a
+rm -rf $legitdir
+
+# 4. when switch the branch , the new file added in the current branch will not exist in the 
+#    branch we switch to 
+std1="Already on 'new'"
+sh legit-init > /dev/null
+touch a 
+sh legit-add a
+sh legit-commit -m "commit" > /dev/null
+perl legit-branch new > /dev/null
+perl legit-checkout new  > /dev/null
+touch b
+sh legit-add b
+sh legit-commit -m "commit" > /dev/null
+perl legit-checkout master > /dev/null
+
+if [ -e "$fullpath/$indexdir/a" ] && [ ! -e "$fullpath/$indexdir/b" ]
+then
+  echo -en "\e[32mfile change PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile change failed\n\e[0m"
+fi
+
+# 5. when switch back to the new , the file b should restore to current dir
+# and index dir 
+perl legit-checkout new > /dev/null
+
+if [ -e "$fullpath/$indexdir/a" ] && [ -e "$fullpath/$indexdir/b" ]  && [  -e a ] && [ -e b ]
+then
+  echo -en "\e[32mfile change retored PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile change resotred failed\n\e[0m"
+fi
+
+# 5. when modified file is also exist in another branch, it will pump out the error
+# message 
+
+perl legit-checkout master> /dev/null
+echo "siyu" >> b
+sh legit-add b
+sh legit-commit -m "message"> /dev/null
+echo "mimi" >> b
+sh legit-add b
+echo "sy" >> b
+perl legit-checkout new >.a
+out1=`cat .a`
+std1="legit-checkout: error: Your changes to the following files would be overwritten by checkout:
+b"
+if [ "$std1" = "$out1" ] 
+then
+  echo -en "\e[32moverwritten check PASSED\n\e[0m"
+else
+  echo -en "\e[31moverwritten check failed\n\e[0m"
+fi
+rm b a
+
 
 rm -rf .legit
 echo -en "\e[35mTESTS FINISHED\n\e[0m"
