57adb09867385cf12f6c0ae6ec88579fb5fc12a9
us579 <wzus579@gmail.com>
2019-07-11 15:20:38 +1000

    fix legit-status: error: your repository does not have any commits yet

--- assignment1/test06.sh
+++ assignment1/test06.sh
@@ -31,19 +31,123 @@ fi
 
 # 2. file changed, different changes staged for commit
 std1='a - file changed, different changes staged for commit';
-echo "hello" > a
+touch a
 sh legit-add a 
 sh legit-commit -m 'commit' > /dev/null
 echo "world" >> a
 sh legit-add a
-$out1=$( sh legit-status )
-$out = `echo $out1 | grep "a - .*"`
+echo "hello" >> a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- file changed.*for commit\).*/\1/")
 if [ "$std1" = "$out" ]
 then
-  echo -en "\e[32minit PASSED\n\e[0m"
+  echo -en "\e[32mfile changed, different changes staged for commit PASSED\n\e[0m"
 else
-  echo -en "\e[31minit failed\n\e[0m"
+  echo -en "\e[31mfile changed, different changes staged for commitfailed\n\e[0m"
 fi
+
+# 2. file changed, changes staged for commit
+sh legit-init > /dev/null
+std1='a - file changed, changes staged for commit';
+touch a
+sh legit-add a
+sh legit-commit -m 'commit' > /dev/null
+echo "world" >> a
+sh legit-add a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- file changed.*for commit\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32mfile changed, changes staged for commit PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile changed, changes staged for commit failed\n\e[0m"
+fi
+rm -rf $legitdir
+
+# 3. file changed, changes not staged for commit
+sh legit-init > /dev/null
+std1='a - file changed, changes not staged for commit';
+touch a
+sh legit-add a
+sh legit-commit -m 'commit' > /dev/null
+echo "world" >> a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- file changed.*not staged for commit\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32mfile changed, changes not staged for commit PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile changed, changes not staged for commit failed\n\e[0m"
+fi
+rm -rf $legitdir a
+
+
+# 3. file deleted
+sh legit-init > /dev/null
+std1='a - file deleted';
+touch a
+sh legit-add a
+sh legit-commit -m 'commit' > /dev/null
+rm a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- file deleted\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32mfile deleted PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile deleted failed\n\e[0m"
+fi
+rm -rf $legitdir
+
+
+# 3. deleted (either current dir or index)
+sh legit-init > /dev/null
+std1='a - deleted';
+touch a
+sh legit-add a
+sh legit-commit -m 'commit' > /dev/null
+sh legit-rm a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- deleted\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32mdeleted PASSED\n\e[0m"
+else
+  echo -en "\e[31mfile deleted failed\n\e[0m"
+fi
+rm -rf $legitdir
+
+# 4. same as repo
+sh legit-init > /dev/null
+std1='a - same as repo';
+touch a
+sh legit-add a
+sh legit-commit -m 'commit' > /dev/null
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- same as repo\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32msame as repo PASSED\n\e[0m"
+else
+  echo -en "\e[31msame as repo failed\n\e[0m"
+fi
+rm -rf $legitdir
+
+# 5. added to index
+sh legit-init > /dev/null
+std1='a - added to index';
+touch a
+sh legit-add a
+out1=$( sh legit-status )
+out=$(echo $out1 | sed "s/.*\(a \- added to index\).*/\1/")
+if [ "$std1" = "$out" ]
+then
+  echo -en "\e[32madded to index PASSED\n\e[0m"
+else
+  echo -en "\e[31madded to index failed\n\e[0m"
+fi
+# rm -rf $legitdir
+
 
 
 
