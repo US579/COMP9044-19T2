6b957b424cf17ade08a6ae84dfbfe2f23887edac
us579 <wzus579@gmail.com>
2019-07-19 15:46:57 +1000

    add weekly test week7

--- weekly_test/week07/largest_numbered_line.pl
+++ weekly_test/week07/largest_numbered_line.pl
@@ -3,19 +3,30 @@ while ($line = <>){
 	@array = split / /, $line;
 	foreach $n (@array){
 		if ( $n =~ /(\-?\d*\.?\d+)/){
-			print $1;
+			# print $1;
 			push @lis , $1
 		}
 	}
 	if ($#lis == -1 ){
 		exit 1;
 	}
-	
-@lis=reverse sort(@lis);
-$num = shift @lis;
-print $num;
-push @compare , $num;
-@compare = reverse sort(@compare);
-$mx = shift @compare;
-print $mx;
+
+	my $num = (sort {$b <=> $a} @lis)[0];
+	# print $num;
+	$dic{$line} = $num;
+	push @compare , $num;
+	@di = keys %dic;
+	# foreach $v (@di){
+	# 	print "$dic{$v} ==> $v";
+	# }
+	@lis = ();
+
+}
+
+my $mx = (sort {$b <=> $a} @compare)[0];
+@di = keys %dic;
+foreach $w (@di){
+	if ( $dic{$w} eq $mx ){
+		print $w;
+	}
 }
