756300aa1031dc674b6994e2bb40c9c3742fe5e7
us579 <wzus579@gmail.com>
2019-07-23 08:37:54 +1000

    add week8 lab

--- LAB/lab08/Excerse03/collapse/collapse.js
+++ LAB/lab08/Excerse03/collapse/collapse.js
@@ -1,3 +1,19 @@
 (function() {
+		const d = document.getElementById('item-1');
+		d.onclick = function(){
+			const dc = document.getElementById('item-1-content');
+
+			dc.style.display = "none";
+		};
+		const d1 = document.getElementById('item-2');
+		d1.onclick = function(){
+			const dc1 = document.getElementById('item-2-content');
+
+			dc1.style.display = "none";
+		};
+
+
+
 
 }());
+		
