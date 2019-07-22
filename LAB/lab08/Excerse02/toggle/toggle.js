eb27d149d5a20693ed985d5134903cd096ddab36
us579 <wzus579@gmail.com>
2019-07-23 08:07:23 +1000

    add week8 lab

--- LAB/lab08/Excerse02/toggle/toggle.js
+++ LAB/lab08/Excerse02/toggle/toggle.js
@@ -1,4 +1,8 @@
 (function() {
    'use strict';
    // write your js here.
+   let docwindow = document.getElementById("output");
+   setInterval(()=>{
+   	docwindow.classList.toggle('hide');
+   },2000);
 }());
