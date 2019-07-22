f22273526550a64f7474edddad6719399d9fb3bb
us579 <wzus579@gmail.com>
2019-07-23 09:10:26 +1000

    add week8 lab

--- LAB/lab08/Excerse04/expand/expand.js
+++ LAB/lab08/Excerse04/expand/expand.js
@@ -1,4 +1,30 @@
 (function() {
-  'use strict';
-  // TODO: Write some js
+		const d = document.getElementById('item-1');
+		d.onclick = function(){
+			const dc = document.getElementById('item-1-content');
+			if (dc.style.display == "block"){
+				dc.style.display = "none";
+				d.innerHTML= "expand_more";
+			}else{
+				dc.style.display = "block";
+				d.innerHTML = "expand_less";
+			};
+
+		};
+		const d1 = document.getElementById('item-2');
+		d1.onclick = function(){
+			const dc1 = document.getElementById('item-2-content');
+			if (dc1.style.display == "block"){
+				dc1.style.display = "none";
+				d1.innerHTML= "expand_more";
+			}else{
+				dc1.style.display = "block";
+				d1.innerHTML= "expand_less";
+			};
+
+			
+		};
+
+
 }());
+		
