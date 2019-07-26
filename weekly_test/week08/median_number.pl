7014255a5ad5fb6e409412015ab6043e7451a296
us579 <wzus579@gmail.com>
2019-07-26 11:18:46 +1000

    add week8 lab exercise1

---
+++ weekly_test/week08/median_number.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use Math::Round;
+@array=();
+foreach my $x (@ARGV) {
+	push @array ,$x;
+}
+
+$size = @array.length();
+$size = $size/2;
+@array=sort(@array);
+$size -= 0.5;
+print "@array[$size]\n";
+
+# foreach my $num (@array){
+# 	print $num;
+# }
