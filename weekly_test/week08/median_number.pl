df517de8b6ec95815431412a6e7278df0408b405
us579 <wzus579@gmail.com>
2019-07-27 08:30:58 +1000

    add week8 lab exercise1

--- weekly_test/week08/median_number.pl
+++ weekly_test/week08/median_number.pl
@@ -6,8 +6,9 @@ foreach my $x (@ARGV) {
 	push @array ,$x;
 }
 
-$size = @array.length();
+
+$size = @array;
 $size = $size/2;
 @array=sort(@array);
 $size -= 0.5;
-print "@array[$size]\n";
+print "$array[$size]\n";
