e48fd50a99331062b0a8ef143bcc341945ca1bd8
us579 <wzus579@gmail.com>
2019-07-30 16:56:55 +1000

    add e2

--- LAB/lab09/pretty_pictures/fetch.js
+++ LAB/lab09/pretty_pictures/fetch.js
@@ -1,3 +1,56 @@
 export default function runApp() {
     /** your code goes here */
+    window.onload = run;
+
+    function createPage(link){
+        var output = document.getElementById("output");
+        let div = document.createElement("div");
+        div.className="img-post";
+        let img = document.createElement("img");
+        img.setAttribute("src",link);
+        let p = document.createElement("p");
+        const time = new Date();
+        p.innerText = `Fetched at ${time.getHours()}:${time.getMinutes()}`;
+        let br = document.createElement("br");
+        div.appendChild(img);
+        div.appendChild(p);
+        output.appendChild(div);
+    }
+ 
+    function getimages(){
+        const url = "https://picsum.photos/300/300/?random";
+        const imagesList = [];
+        for (let i = 0 ;i < 5;++i){
+            imagesList.push(fetch(url))
+        }
+        Promise.all(imagesList)
+        .then((Response)=>{
+            Response.map((x)=>createPage(x.url));
+        })
+
+
+    }
+    function removeImages(){
+        const child = [];
+        for (let node of output.childNodes){
+            console.log(node.baseURI.substring(0, 4));
+            if (node.baseURI.substring(0, 4)==="http"){
+                console.log(node);
+                child.push(node);
+            }
+        }
+        alert(child.length);
+        for (let i =0 ; i < child.length;++i){
+            output.removeChild(child[i]);
+        }
+    }
+
+    function run() {
+        var mouse = document.getElementById("more");
+        mouse.addEventListener('click', (event) => {
+            removeImages();
+            getimages();
+        })
+    }
 }
+
