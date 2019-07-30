5674149c90d265f574cf99fc7adeb02a28020f86
us579 <wzus579@gmail.com>
2019-07-30 15:30:58 +1000

    add e1

--- LAB/lab09/fetch_and_list/fetch.js
+++ LAB/lab09/fetch_and_list/fetch.js
@@ -1,3 +1,28 @@
 export default function runApp() {
     /** your code goes here */
+window.onload=run;
+function pageCreate(user) {
+	let div = document.createElement('div'); 
+	div.className = 'user'; 
+	let h2 = document.createElement('h2'); 
+	h2.innerText = user.name; 
+	let p = document.createElement('p'); 
+	p.innerText = user.company.catchPhrase;
+	div.appendChild(h2); 
+	div.appendChild(p); 
+	return div;
+} 
+function append(element) {
+	output.appendChild(element); 
+} 
+function run() {
+	var output = document.getElementById('output');
+	fetch('https://jsonplaceholder.typicode.com/users') 
+	.then(res => res.json()) 
+	.then(data => data.map(pageCreate))
+	.then(elements => elements.map(append));
 }
+};
+
+
+
