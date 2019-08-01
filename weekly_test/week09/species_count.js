1d7fb312bb534b231dc72b48f23cb36f08abda77
us579 <wzus579@gmail.com>
2019-08-02 08:21:13 +1000

    finish e2

--- weekly_test/week09/species_count.js
+++ weekly_test/week09/species_count.js
@@ -1,7 +1,11 @@
 function species_count(target_species, whale_list) {
-
-  // PUT YOUR CODE HERE
-
+	var su = 0;
+	for (let i=0;i<whale_list.length;++i){
+		if (whale_list[i].species == target_species){
+			su += +whale_list[i].how_many;
+		}
+	}
+  	return su;
 }
 
 module.exports = species_count;
