89da4dcffcfc933d3131c18bec9a9613058ce494
us579 <wzus579@gmail.com>
2019-08-02 08:07:14 +1000

    finish e1

--- weekly_test/week09/sum.js
+++ weekly_test/week09/sum.js
@@ -1,4 +1,10 @@
 function sum(list) {
+	dic={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"0":0}
+	var su = 0;
+	for (let i =0;i< list.length ;++i){
+		su = su + dic[list[i]]; 
+	}
+	return su;
 
   // PUT YOUR CODE HERE
 
