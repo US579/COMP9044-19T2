2849a2793e62048a747ffc7f683db185db826c70
us579 <wzus579@gmail.com>
2019-08-04 10:18:18 +1000

    fix sum

--- weekly_test/week09/sum.js
+++ weekly_test/week09/sum.js
@@ -1,8 +1,7 @@
 function sum(list) {
-	dic={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"0":0}
 	var su = 0;
 	for (let i =0;i< list.length ;++i){
-		su = su + dic[list[i]]; 
+		su += +list[i]; 
 	}
 	return su;
 
