b8252161a6c70f2af41889aead9329e14f55436b
z5137189 <z5137189@cse.unsw.edu.au>
2019-08-07 18:47:58 +1000

    commit

--- final/q5.pl
+++ final/q5.pl
@@ -1,5 +1,23 @@
 #!/usr/bin/perl -w 
 
 @array=@ARGV;
+#print @array;
+$one = shift @array;
+#print $one;
+$var{$one} = 1;
 
-print @array;
+@arr = keys %var;
+#print @arr;
+
+print "$arr[0] ";
+foreach $word ( @ARGV ){
+        if ( exists $var{$word} ){
+#            print "$word\n";
+            next;
+        }else{
+            $var{$word} = 1;
+           print "$word "
+        }
+}
+print "\n";
+
