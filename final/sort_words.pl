3b62f917e880641628ab0039a25a9dc64706c737
us579 <wzus579@gmail.com>
2019-08-08 11:50:11 +1000

    add

--- final/sort_words.pl
+++ final/sort_words.pl
@@ -1,16 +1,9 @@
 #!/usr/bin/perl -w
 
-#while ( <> ){
-#    push @array , $_;
-#}
-
 @array = <STDIN>;
 
-
-foreach  ( @array ){
-    @array = split (/\s+/,$_);
-    @array = sort(@array);
-    print "@array\n";
-    #    pop @array;
-    #print join " ", @array;    
+foreach $w (@array){
+    @arr=split (/\s+/,$w);
+    @arr=sort (@arr);
+    print "@arr\n";
 }
