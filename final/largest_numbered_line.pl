93609cd1c36959a81a82e5cd65db39eef1f3556d
us579 <wzus579@gmail.com>
2019-08-10 11:41:00 +1000

    add

--- final/largest_numbered_line.pl
+++ final/largest_numbered_line.pl
@@ -1,3 +1,26 @@
 #!/usr/bin/perl -w
 
 @arr=<>;
+
+
+
+foreach $line  (@arr){
+    @nums = $line =~ /\-?(?:\d+\.?\d*|\d*\.\d+)/g;
+    #print @nums;
+    if (@nums){
+    @sort_nums = sort{$b<=>$a} @nums;
+    $larg=$sort_nums[0];
+    $dic{$line} = $larg;
+    push @numarr ,$larg;
+}
+}
+
+$bignum = (sort{$a<=>$b} @numarr)[0];
+
+@val = values %dic;
+foreach $w (keys %dic){
+    if ($dic{$w} == $bignum){
+        print $w;
+    }
+    
+}
