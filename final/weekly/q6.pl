a17f6b6d8d1f9de5f95f5083e835370de907628c
us579 <wzus579@gmail.com>
2019-08-12 18:34:46 +1000

    add q6 answer

--- final/weekly/q6.pl
+++ final/weekly/q6.pl
@@ -4,18 +4,24 @@
 @arr=<>;
 
 foreach $line (@arr){
+    #将所有小数点为4的去除
     $line =~ s/\.[0-4][0-9]*//g;
     $temp = $line;
-    @nums = $line =~ /([0-9]+\.[5-9]+)/g;
+    #把line里面大于.5的数字提取出来
+    @nums = $line =~ /([0-9]+\.[5-9]+[0-9*])/g;
     if (@nums){
         foreach $n (@nums){
+            # 保存pattern
             $tempp = $n;
+            # 将大于.5的数字后的小数去掉,只提取整数部分
             $n =~ s/\..*//;
-            print "$n\n";
+            # 整数部分+1
             $update_n = $n + 1;
-            print $update_n;
+            # 将pattern替换成+1后的数,不能用g,要一个一个替换,列表里的顺序和字符串里的顺序一致
             $temp =~ s/$tempp/$update_n/; 
         }
-        print "$temp\n"
+        print "$temp";
+        next;
     }
+    print $line;
 }
